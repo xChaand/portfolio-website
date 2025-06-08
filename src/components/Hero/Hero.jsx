6ce3b2a009ad4e7174e3fafb472b911e283a3f64
Chaand <126508848+xChaand@users.noreply.github.com>
2025-06-08 16:13:28 +0300

    Update Resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -55,7 +55,7 @@ export default function Hero() {
               <LinkedInIcon />
             </a>
             <a
-              href="https://drive.google.com/file/d/1CPJLFgUchNdHKOwHuYawn4JZQkGb3IcJ/view?usp=sharing"
+              href="https://drive.google.com/file/d/1tQHSgSBX6SodJUg_2b4ThzreUTUmWoe5/view?usp=sharing"
               target="_blank"
               className="bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm p-2 sm:p-4 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
             >
