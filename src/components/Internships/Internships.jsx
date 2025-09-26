c9c1501f61e8955449e8e398fa8bcc4f38d926aa
Chaand Bakshi <cbakshi@wisc.edu>
2025-09-26 00:54:57 -0500

    created Internship.jsx page

---
+++ src/components/Internships/Internships.jsx
@@ -0,0 +1,9 @@
+export default function Internships() {
+  return (
+    <div className="w-screen">
+      <div className="h-screen p-16 flex flex-col items-start justify-start md:justify-center">
+        Intern
+      </div>
+    </div>
+  );
+}
